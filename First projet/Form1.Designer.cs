87661c41d3378a99f75e626ff1a257b067b671f5
DEVPRO278 <soltan.nb93@gmail.com>
2017-02-01 19:34:50 +0000

    F up

---
+++ First projet/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace First_projet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.button1.Location = new System.Drawing.Point(302, 358);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "affichier";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(90, 57);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(234, 212);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(457, 58);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(243, 212);
+            this.listBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.Cornsilk;
+            this.button2.Location = new System.Drawing.Point(90, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "open cnx";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.Color.Cornsilk;
+            this.button3.Location = new System.Drawing.Point(525, 406);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 52);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "close cnx";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(365, 150);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = ">>";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::First_projet.Properties.Resources.apple_logo_018;
+            this.ClientSize = new System.Drawing.Size(728, 520);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Cornsilk;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ First projet/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace First_projet
+{
+    public partial class Form1 : Form
+    {
+
+       SqlConnection cnx = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=C:\AA\base de stagaire.mdf;Integrated Security=True;Connect Timeout=30;User Instance=True"); 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            cnx.Open();
+            SqlCommand cmd = new SqlCommand("select * from STG ", cnx);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read() == true )
+            { 
+            
+                listBox1.Items.Add (dr["num_stg"]);
+
+            
+            }
+
+
+          cnx.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (cnx.State.ToString () == "open")
+            {
+                MessageBox.Show(" conix daija open ");
+            }
+            else
+            {
+
+                cnx.Open();
+                MessageBox.Show("STATE : " + cnx.State.ToString() + " ---- " + " MSG : sucsusfuly ");
+                cnx.Close();
+            }
+            
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cnx.Close();
+            MessageBox.Show("STATE : " + cnx.State.ToString() + " ---- " + " MSG : sucsusfuly ");
+      
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+
+            cnx.Open();
+            SqlCommand cmd = new SqlCommand("select * from STG ", cnx);
+
+            SqlDataReader dr = cmd.ExecuteReader();
+           // ListBox l1 = new ListBox();
+
+            while (dr.Read())
+            {
+                if ( dr["num_stg"].Equals ( listBox1.SelectedItem ) )
+                {
+                    string x = (Convert.ToString(dr["num_stg"]) + '-' + dr["nom_stg"] + '-' + dr["prenom_stg"] + '-' + dr["fillier"]);
+                   listBox2.Items.Add ( x );
+                   
+                    break;
+                }
+                 
+
+            }
+
+
+            cnx.Close();
+
+        }
+    }
+}
